894165f9197607113e94c8959c0058049cbfa012
Marina90 <miss.honeymari@gmail.com>
2022-08-23 13:14:07 +0300

    все задачи

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (n2 > max ) max = n2;
 Console.Write("max = ");
 Console.WriteLine(max);
 
-*/
+
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 Console.Write( "input first number: " );
@@ -29,3 +29,23 @@ if (n3 > max ) max = n3;
 Console.Write("max = ");
 Console.WriteLine(max);
 
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) Console.WriteLine("да");
+else Console.WriteLine("нет");
+Console.ReadKey();
+
+*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.Read());
+int current = num % 2;
+while (current == 0)
+{Console.Write(current + " ");
+current++;
+} 
